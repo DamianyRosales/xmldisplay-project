b5049e559f91a15f08adb119bbb0a11f8c1535ce
Damiany Rosales <damianyrosales@gmail.com>
2022-10-14 10:56:49 -0500

    cors headers

--- users/views.py
+++ users/views.py
@@ -2,53 +2,52 @@ from users.models import Tab
 import  xml.etree.ElementTree as xmltree
 from xml.dom import minidom
 from django.http import HttpResponse
+from rest_framework.views import APIView
 
 # Create your views here.
 
 save_path_file = "file.xml"
 
-def my_view(request):
+def my_view(APIView):
     
-    root = xmltree.Element('Tabs')
-    current_year=0
-    for i in Tab.objects.all():
-        if current_year != i.year:
-            current_year = i.year
-            yy = 'year'+str(current_year)
-            year = xmltree.Element(yy)
-            root.append(year)
 
-        tab_month = xmltree.SubElement(year, str(i.month))
+    def get(request, format=None):
+        root = xmltree.Element('Tabs')
+        current_year=0
+        for i in Tab.objects.all():
+            if current_year != i.year:
+                current_year = i.year
+                yy = 'year'+str(current_year)
+                year = xmltree.Element(yy)
+                root.append(year)
 
-        tab_food = xmltree.SubElement(tab_month, 'FOOD')
-        tab_food.text = str(i.food)
+            tab_month = xmltree.SubElement(year, str(i.month))
 
-        tab_brewery = xmltree.SubElement(tab_month, 'BREWERY')
-        tab_brewery.text = str(i.brewery)
+            tab_food = xmltree.SubElement(tab_month, 'FOOD')
+            tab_food.text = str(i.food)
 
-        tab_chemical_products = xmltree.SubElement(tab_month, 'CHEMICAL_PRODUCTS')
-        tab_chemical_products.text = str(i.chemical_products)
+            tab_brewery = xmltree.SubElement(tab_month, 'BREWERY')
+            tab_brewery.text = str(i.brewery)
 
-        tab_other_manufactures = xmltree.SubElement(tab_month, 'OTHER_MANUFACTURES')
-        tab_other_manufactures.text = str(i.other_manufactures)
+            tab_chemical_products = xmltree.SubElement(tab_month, 'CHEMICAL_PRODUCTS')
+            tab_chemical_products.text = str(i.chemical_products)
 
-        tab_textiles_leather = xmltree.SubElement(tab_month, 'TEXTILES_AND_LEATHER')
-        tab_textiles_leather.text = str(i.textiles_leather)
+            tab_other_manufactures = xmltree.SubElement(tab_month, 'OTHER_MANUFACTURES')
+            tab_other_manufactures.text = str(i.other_manufactures)
 
-        tab_total = xmltree.SubElement(tab_month, 'TOTAL')
-        tab_total.text = str(i.total)
+            tab_textiles_leather = xmltree.SubElement(tab_month, 'TEXTILES_AND_LEATHER')
+            tab_textiles_leather.text = str(i.textiles_leather)
 
-
-    #tree = xmltree.ElementTree(root)
-    xmlstr = minidom.parseString(xmltree.tostring(root).decode("utf-8")).toprettyxml(indent="\t")
-    #xmlstr = minidom.parseString(xmltree.tostring(root).decode())
-    #xmlstr = xmltree.tostring(root).decode()
-    #xmlstr = minidom.parse(xmlstr).toprettyxml(indent="\t")
-
-    with open ('file.xml', "w") as files :
-        files.write(xmlstr)
+            tab_total = xmltree.SubElement(tab_month, 'TOTAL')
+            tab_total.text = str(i.total)
 
 
-    #return HttpResponse(open('file.xml').read(), content_type='application/xml')
-    html = '<pre>' + open('file.xml').read() + '</pre>'
-    return HttpResponse(html)
+        xmlstr = minidom.parseString(xmltree.tostring(root).decode("utf-8")).toprettyxml(indent="\t")
+
+        with open ('file.xml', "w") as files :
+            files.write(xmlstr)
+
+
+        #return HttpResponse(open('file.xml').read(), content_type='application/xml')
+        #html = '<pre>' + open('file.xml').read() + '</pre>'
+        #return HttpResponse(html)
--- xmlfilecreation/settings.py
+++ xmlfilecreation/settings.py
@@ -37,15 +37,52 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'users',
+    'rest_framework',
     'django_extensions',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "cache-control",
+    "pragma",
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework_xml.parsers.XMLParser',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework_xml.renderers.XMLRenderer',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
